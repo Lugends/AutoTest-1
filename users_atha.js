55dcb87e0a0b1f57f6f0d92e20f937214bd91ad5
Lugends <118277094+Lugends@users.noreply.github.com>
2023-01-10 09:07:42 +0300

    Update users_atha.js

--- users_atha.js
+++ users_atha.js
@@ -3,6 +3,6 @@ import { faker } from '@faker-js/faker';
 export const  users_atha =
 {
 users: {
-    email: "Edgar.32@yandex.ru",// В ковычках меняем знчения 
-    password: "wLqcTKIn6vIqPBH"// В ковычках меняем знчения 
+    email: "Edgar.32@yandex.ru",// В ковычках меняем знчение 
+    password: "wLqcTKIn6vIqPBH"// В ковычках меняем знчение
 }}
